fca464e5cca2beaa2afaa23694f24c1b8b29393a
tabesbridges <tabesbridges@gmail.com>
2022-12-13 02:11:17 -0600

    Restructure code and finish dimension change feature.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+// Obtain root, which allows access to CSS variables.
+let CSSroot = document.querySelector(':root');
+
 function getDimension() {
     do {
-        input = parseInt(prompt("Enter a number between 16 and 100!", "16"))
+        input = parseInt(prompt("Enter a number between 1 and 100!", "16"))
     } while (
-    (input < 16) || (input > 100)
+    (input < 1) || (input > 100)
     );
     return input;
 }
-
-
 
 function generateGrid() {
     // First, empty out the old grid if it exists
@@ -16,11 +17,16 @@ function generateGrid() {
         box.remove();
     });
 
+    const container = document.querySelector('#container');
+
     dimension = getDimension();
 
-    const container = document.querySelector('#container');
-    // The idea is to make dim^2 divs and use CSS to space them correctly
+    // Call the setCSSvar function to update the CSS
+    setCSSvar('--box-size', dimension);
 
+    // The idea is to make dim^2 divs distributed equally over dim
+    // intermediate divs.
+    
     for (i = 0; i < (dimension ** 2); i++) {
         let box = document.createElement('div');
         const box_id = document.createAttribute('class');
@@ -28,18 +34,35 @@ function generateGrid() {
         box.setAttributeNode(box_id);
         container.appendChild(box);
 
-        // Add event listener to the box
+        // Add event listener to the box (not using CSS variables here
+        // since we would need one for each box)
         box.addEventListener('mouseover', (event) => {
             event.target.style.backgroundColor = "purple";
 
             // reset color after 500ms
             setTimeout(() => {
                 event.target.style.backgroundColor = "aqua"
-              }, 500);
+            }, 500);
         }, false);
     }
-    return true
 }
+
+// Create a function for getting a variable value
+function getCSSvar(target) {
+    // Get the styles (properties and values) for the root
+    var rootStyles = getComputedStyle(CSSroot);
+    // return the value of the target variable
+    return rootStyles.getPropertyValue(target);
+  }
+
+// Create a function for setting a variable value
+function setCSSvar(target, newValue) {
+    // Set the value of target variable to another value
+    CSSroot.style.setProperty(target, newValue);
+  }
+
+
+
 
 document.addEventListener("DOMContentLoaded", function() {
     generateGrid();
